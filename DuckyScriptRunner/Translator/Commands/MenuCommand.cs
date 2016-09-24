a17c8d98aaac31c85fa093d6a0d4c280eecafc09
arveh4 <arveh4@online.no>
2016-09-24 18:03:01 +0200

    Added MENU

---
+++ DuckyScriptRunner/Translator/Commands/MenuCommand.cs
@@ -0,0 +1,14 @@
+﻿using WindowsInput;
+using WindowsInput.Native;
+
+namespace DuckyScriptRunner.Translator.Commands
+{
+    public class MenuCommand: DuckyCommandBase
+    {
+        public override string Name { get; } = "MENU";
+        public override void Execute(IKeyboardSimulator keyboard)
+        {
+            keyboard.ModifiedKeyStroke(VirtualKeyCode.SHIFT, VirtualKeyCode.F10);
+        }
+    }
+}
